0541fbdafa0c4f68d6d8a094b8096b183c9d1b55
Indra Teja Chintakayala <ichintakayala@unomaha.edu>
2019-06-12 15:25:03 -0500

    Changes to home and upload function

--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
+def upload(request):
+    return HttpResponse("You're at the upload index.")
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def home(request):
+    return render(request, 'home.html',
+    {'home': home})
